ee319deb483384cb3c463404c9f3453c30edc3fd
Julien Monat <julien.monat.rodier@gmail.com>
2012-01-31 12:08:21 -0500

    - tweaked visuals - added a hide blog button - removed background image from blogger template

--- Scripts/Blog/blog.js
+++ Scripts/Blog/blog.js
@@ -3,17 +3,33 @@ Copyright (C) 2012 Julien Monat-Rodier
 Licence in LICENCE.txt
 */
 
-
-﻿
 function FormatPage()
 {
     document.body.style.padding = "0 0 0px";
     document.body.style.margin = "0px";
 
-    var navBar = document.getElementById("navbar-iframe");
-    if (!navBar)
-        return;
-
     var game = document.getElementById("game");
     game.focus();
+    
+    ToggleBlog( document.getElementById("visibleToggle") );
+}
+
+function ToggleBlog(item)
+{
+    if (item.checked)
+    {
+        var blog = document.getElementById("body_container");
+        blog.style.visibility = "visible";
+        
+        var buttonText = document.getElementById("buttonText");
+        buttonText.innerHTML = "Show Game";
+    }
+    else
+    {
+        var blog = document.getElementById("body_container");
+        blog.style.visibility = "hidden";
+        
+        var buttonText = document.getElementById("buttonText");
+        buttonText.innerHTML = "Show Blog";
+    }
 }
